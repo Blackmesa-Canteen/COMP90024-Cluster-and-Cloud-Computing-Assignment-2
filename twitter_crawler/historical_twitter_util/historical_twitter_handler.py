e45d9f7b5da44a62d4f3f2f88b4f82d2eea27fcb
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-04-26 00:11:09 +1000

    change logger for exception handling

--- twitter_crawler/historical_twitter_util/historical_twitter_handler.py
+++ twitter_crawler/historical_twitter_util/historical_twitter_handler.py
@@ -50,7 +50,7 @@ class HistoricalTwitterHandler:
                     break
                 except Exception as e:
                     print(original_tweet_dict)
-                    logger.error(e)
+                    logger.exception(e)
                     break
 
     def parse_twitter_with_keyword_and_put_to_db(self):
